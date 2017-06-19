9665a4ed053a71f10e0037f216a5125df1061ba8
ejdna5 <ejaehnig@gmail.com>
2017-06-19 03:03:30 +0000

    first draft

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import pandas as pd
+from bokeh.io import show
+from bokeh.layouts import row
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+def create_plot(company):
+  tick=company.upper()
+  apiKey='DsfYw6GwZRN_j6Pq_cSW'
+  startD='20170519'
+  endD='20170618'
+  getURL="https://www.quandl.com/api/v3/datasets/WIKI/%s/data.json" %tick
+  para="&api_key=%s&start_date=%s&end_date%s" % (apiKey,startD,endD)
+  r=requests.get(getURL,para)
+  r1=r.json()
+  rdf=pd.DataFrame(r1['dataset_data']['data'],columns=r1['dataset_data']['column_names'])
+  rdf['Date'] = pd.to_datetime(rdf['Date'])
+  p = figure(title="Closing price for each day of the past month", plot_height=300, plot_width=600)
+  r = p.line(rdf['Date'], rdf['Close'], color="#2222aa", line_width=3)
+  return r
 
 app = Flask(__name__)
 
@@ -8,7 +29,14 @@ def main():
 
 @app.route('/index')
 def index():
-  return render_template('index.html')
+  current_ticker = request.args.get("ticker")
+  if current_ticker == None:
+    current_ticker = "GOOG"
+  plot = create_plot(ticker)
+		
+  # Embed plot into HTML via Flask Render
+  script, div = components(plot)
+  return render_template("index.html", script=script, div=div, current_ticker=current_ticker)
 
 if __name__ == '__main__':
   app.run(port=33507)
